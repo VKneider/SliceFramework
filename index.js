815a1a4a47e37f1bbd7a54c3ae2bb25390134c63
Victor Kneider <victorkneider@gmail.com>
2023-02-05 02:31:39 -0400

    feat: trying new instances of card

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 
 let x = async function () {
-    let nav_bar = await slice.getInstance("./js/NavBar.js")
-    let nav_bar2 = await slice.getInstance("./js/NavBar.js")
+    //let nav_bar = await slice.getInstance("./js/NavBar.js")
+    let card = await slice.getInstance("./js/Card.js", { 
+    title: "Google requiere empleados",
+    subtitle: "Google C.A",
+    description: "hola soy google y te gua comprar ",
+    image: " https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+    redirect: "https://www.google.com" 
+    })
+
+    let card2 = await slice.getInstance("./js/Card.js")
+    let card3 = await slice.getInstance("./js/Card.js",     {title:"Game of Thrones", subtitle:"HBO", description:"hola soy google y te gua comprar ", image:" https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", redirect:"https://www.google.com"})
+
+
+    let div = document.createElement("div")
+    div.classList.add("grid")
+
+
+    div.appendChild(card)
+    div.appendChild(card2)
+    div.appendChild(card3)
+    document.body.appendChild(div)
+    
+    
 }
 
 x();
