34da2407d0856e331539b1639ec41c0c9c344d55
Galuito <nluisgalue20@hotmail.com>
2023-02-05 20:20:52 -0400

    getInstance (module, props) -> (name, props)

--- Slice/Slice.js
+++ Slice/Slice.js
@@ -23,11 +23,20 @@
             
         }
 
-        async getInstance(module, props){
-            //modulo es el nombre del modulo js
-            let name = module.split("/").pop().split(".")[0];
+        async getInstance(name, props){ //Antes era module en vez de name en esta linea
+            let module = "./js/" + name + ".js";
+            //Hay que pasar el nombre con la primera letra mayúscula.
+
+            //Vieja función que obtenía el nombre a través del módulo
+            //let name = module.split("/").pop().split(".")[0];
             
-            console.log(props)
+            //Cambie el comportamiento de esto, los props solo se van a imprimir a la consola si existen
+            //igualmente, esto debería de retirarse ya que lo único que logra es hacer que la consola esté
+            //más llena de basura y en algún punto podría llegar a ser contraproducente.
+            if(props != undefined){
+                console.log(props);
+            }
+
             if(this.classes.has(name)){
                 let m = this.classes.get(name);
                 console.log(`Instancia de mapa`);
@@ -61,12 +70,7 @@ async function load(){
 
 }
 
-
-
 load();
-
-
-
 
 function isEmpty(value) {
     return value === undefined || value === null || value === '';
--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-//Small Comment that nobody will notice.
+/**
+ * In this code what Victor did is that he tried to work with only js, so that whenever you try to put a new
+ * element into the body of your program you have to add it through Javascript and DOM manipulation, first creating
+ * the element or a group of elements, then adding that element into a div and finally appending that div into the DOM
+ */
+
+
+/**
+ * different elements that you should be able to use.
+ * Card
+ * img-carousel
+ * Navbar
+ * SliderPro
+ */
+
 
 let x = async function () {
     //let nav_bar = await slice.getInstance("./js/NavBar.js")
-    let card = await slice.getInstance("./js/Card.js", { 
-    title: "Google requiere empleados",
-    subtitle: "Google C.A",
-    description: "hola soy google y te gua comprar ",
-    image: " https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
-    redirect: "https://www.google.com" 
-    })
+    let card = await slice.getInstance("Card", { 
+        title: "Google requiere empleados",
+        subtitle: "Google C.A",
+        description: "hola soy google y te gua comprar ",
+        image: " https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+        redirect: "https://www.google.com" 
+    });
 
-    let card2 = await slice.getInstance("./js/Card.js")
-    let card3 = await slice.getInstance("./js/Card.js",     {title:"Game of Thrones", subtitle:"HBO", description:"hola soy google y te gua comprar ", image:" https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", redirect:"https://www.google.com"})
+    let card2 = await slice.getInstance("Card"); //Este es el elemento con props undefined
+    let card3 = await slice.getInstance("Card",{
+        title:"Game of Thrones", 
+        subtitle:"HBO", 
+        description:"hola soy google y te gua comprar ", 
+        image:" https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", 
+        redirect:"https://www.google.com"
+    });
 
 
     let div = document.createElement("div")
     div.classList.add("grid")
 
-
+    //div.appendChild(nav_bar)
     div.appendChild(card)
     div.appendChild(card2)
     div.appendChild(card3)
