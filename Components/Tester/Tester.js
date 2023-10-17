6267b8feef34e3f2880ef06ad6f60a79d4acbffb
Victor Kneider <victorkneider@gmail.com>
2023-10-16 20:57:50 -0400

    add translator component and update code changes

--- Components/Tester/Tester.js
+++ Components/Tester/Tester.js
@@ -1,15 +1,31 @@
-export default class Tester extends HTMLElement{
-    constructor(props){
-        super();
-        slice.controller.loadTemplate(this)
-
-        let sliceBtn = document.getElementById('slice-btn');
-        //sliceBtn.innerHTML="XD"
-
-    slice.controller.toRegister(this);
-
-
+export default class Tester extends HTMLElement {
+  
+  
+  constructor(props) {
+    super(); 
+    
+    for (const prop in props) {
+      this[prop] = props[prop];
     }
+     
+  }
+  
+  static observedAttributes = ['subject', 'description'];
+
+   attributeChangedCallback(attributeName, oldValue, newValue){
+      if(Tester.observedAttributes.includes(attributeName)){
+
+        switch(attributeName){
+          case 'subject':
+            this.querySelector(".slice_tester_subject").textContent=newValue;
+            break;
+
+          case 'description':
+            this.querySelector(".slice_tester_description").textContent=newValue;
+            break;
+        }
+      }
+   }
 }
 
-customElements.define('slice-tester', Tester);
+customElements.define("slice-tester", Tester);
--- Components/complexNavbar/complexNavbar.js
+++ Components/complexNavbar/complexNavbar.js
@@ -1,63 +1,48 @@
 export default class complexNavbar extends HTMLElement {
+  constructor(props) {
+    super();
+    slice.controller.loadTemplate(this);
+    let menu = this.shadowRoot.querySelector("#menu-icon");
+    let navbar = this.shadowRoot.querySelector(".navbar");
 
-    constructor(props) {
-        super();
-        slice.controller.loadTemplate(this)
-        let menu = this.shadowRoot.querySelector("#menu-icon")
-        let navbar = this.shadowRoot.querySelector(".navbar")
+    menu.addEventListener("click", () => {
+      navbar.classList.toggle("open");
+      menu.classList.toggle("fa-navicon");
+      menu.classList.toggle("fa-close");
+    });
 
-        menu.addEventListener("click", () => {
-            navbar.classList.toggle("open");
-            menu.classList.toggle("fa-navicon");
-            menu.classList.toggle("fa-close");
-        })
+    if (props != undefined) {
+      if (props.id != undefined) {
+        this.id = props.id;
+      }
 
-        if (props != undefined) {
+      let logoLink = this.shadowRoot.getElementById("logoLink");
+      if (props.logoLink != undefined) {
+        logoLink.href = props.logoLink;
+      }
 
-            if (props.id != undefined) {
-                this.id = props.id;
-            }
+      let sign = this.shadowRoot.querySelector("#sign-in");
+      if (props.sign != undefined) {
+        sign.href = props.sign;
+      } else {
+        sign.style.display = "none";
+      }
 
-            let logoLink = this.shadowRoot.getElementById("logoLink");
-            if (props.logoLink != undefined) {
-                logoLink.href = props.logoLink;
-            }
+      let logo = this.shadowRoot.querySelector("#logo");
+      logo.innerHTML = props.logo;
 
-            let sign = this.shadowRoot.querySelector("#sign-in");
-            if (props.sign != undefined) {
-                sign.href = props.sign;
-            } else {
-                sign.style.display = "none";
-            }
-
-
-            let logo = this.shadowRoot.querySelector("#logo");
-            logo.innerHTML = props.logo;
-
-            let titleContainer = this.shadowRoot.querySelector(".navbar");
-            for (let i = 0; i < props.sections.length; i++) {
-                let prop = props.sections[i];
-                titleContainer.innerHTML += (`<li><a id=${prop.id}  href="${prop.link}">${prop.text}</a></li>`)
-            }
-        }
-        slice.controller.toRegister(this);
-
-
+      let titleContainer = this.shadowRoot.querySelector(".navbar");
+      for (let i = 0; i < props.sections.length; i++) {
+        let prop = props.sections[i];
+        titleContainer.innerHTML += `<li><a id=${prop.id}  href="${prop.link}">${prop.text}</a></li>`;
+      }
     }
+    slice.controller.registerComponent(this);
+  }
 
-
-    getElement(id) {
-        return this.shadowRoot.getElementById(id);
-    }
-
-
-
-
+  getElement(id) {
+    return this.shadowRoot.getElementById(id);
+  }
 }
 
-
-window.customElements.define('complex-navbar', complexNavbar);
-
-
-
-
+window.customElements.define("complex-navbar", complexNavbar);
